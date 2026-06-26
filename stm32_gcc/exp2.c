8eb644c53cc068e7ee540509596476523bac671f
yuoek <2055048783@qq.com>
2026-06-26 13:11:56 +0800

    stm32

---
+++ stm32_gcc/exp2.c
@@ -0,0 +1,59 @@
+//
+// #include "stm32f10x.h"
+//
+// uint16_t CountOfToggle = 0;
+//
+// void PA8_LED_Init(void)
+// {
+//     GPIO_InitTypeDef GPIO_InitStructure;
+//     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+//     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+//     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+//     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//     GPIO_Init(GPIOA, &GPIO_InitStructure);
+//     GPIO_SetBits(GPIOA, GPIO_Pin_8);
+// }
+//
+// void TIM2_INT_Init(void)
+// {
+//     TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+//     NVIC_InitTypeDef NVIC_InitStructure;
+//     RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+//
+//     TIM_TimeBaseStructure.TIM_Prescaler = 7199;
+//     TIM_TimeBaseStructure.TIM_Period = 1079; 1666 -> 0.5s 4999 -> 1.5s
+//     TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+//     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+//     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+//
+//     TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+//     TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+//
+//     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+//     NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+//     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+//     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+//     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+//     NVIC_Init(&NVIC_InitStructure);
+//
+//     TIM_Cmd(TIM2, ENABLE);
+// }
+//
+// void TIM2_IRQHandler(void)
+// {
+//     if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+//     {
+//         TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+//         GPIO_WriteBit(GPIOA, GPIO_Pin_8, (BitAction)(!GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_8)));
+//         CountOfToggle++;
+//     }
+// }
+//
+// int main(void)
+// {
+//     PA8_LED_Init();
+//     TIM2_INT_Init();
+//     while(1)
+//     {
+//     }
+// }
---
+++ stm32_gcc/led.c
@@ -0,0 +1,33 @@
+//
+// #include "stm32f10x.h"
+//
+// void LED0_On(void)
+// {
+//     GPIO_ResetBits(GPIOA, GPIO_Pin_8);
+// }
+//
+// void LED0_Off(void)
+// {
+//     GPIO_SetBits(GPIOA, GPIO_Pin_8);
+// }
+//
+// void Delay(unsigned long x)
+// {
+//     unsigned long i;
+//     for(i = 0; i < x; i++);
+// }
+//
+//
+//
+// int main(void)
+// {
+//     LED0_Config();   
+//     while(1)         
+//     {
+//         LED0_On();                
+//         Delay(0x3FFFFF);          
+//         LED0_Off();               
+//         Delay(0x3FFFFF);          
+//     }
+// }
+//
